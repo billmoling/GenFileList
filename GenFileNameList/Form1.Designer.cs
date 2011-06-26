ba8da65f87a499ec0039ae0b88d4221db2b0a023
billmo <panan.songyu@gmail.com>
2011-06-26 23:50:02 +0800

    load file and insert to sqlite DB.
    Signed-off-by: billmo <panan.songyu@gmail.com>

--- GenFileNameList/Form1.Designer.cs
+++ GenFileNameList/Form1.Designer.cs
@@ -29,29 +29,24 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.txt_prefix = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(34, 11);
+            this.textBox1.Location = new System.Drawing.Point(239, 24);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(233, 21);
             this.textBox1.TabIndex = 0;
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(34, 36);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox2.Size = new System.Drawing.Size(351, 216);
-            this.textBox2.TabIndex = 1;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(292, 11);
+            this.button1.Location = new System.Drawing.Point(492, 24);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 21);
             this.button1.TabIndex = 2;
@@ -59,16 +54,54 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // txt_prefix
+            // 
+            this.txt_prefix.Location = new System.Drawing.Point(34, 24);
+            this.txt_prefix.Name = "txt_prefix";
+            this.txt_prefix.Size = new System.Drawing.Size(186, 21);
+            this.txt_prefix.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(297, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FilePath";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "RenamePrefix";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(576, 309);
+            this.dataGridView1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 263);
+            this.ClientSize = new System.Drawing.Size(600, 386);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_prefix);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Scan";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,8 +110,11 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_prefix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- GenFileNameList/Form1.cs
+++ GenFileNameList/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 using System.Globalization;
+using System.Data.SQLite;
+using System.Data.Common;
 
 namespace GenFileNameList
 {
@@ -30,34 +32,123 @@ namespace GenFileNameList
             
 
             FileInfo[] fis = di.GetFiles("*.*",SearchOption.AllDirectories);
+            Dictionary<string, string> fileNameWithDesciption = new Dictionary<string, string>();
+
+
             List<string> nameList = new List<string>();
            
             for (int i = 0; i < fis.Length; i++)
             {
-                nameList.Add(fis[i].Name);
-            }
+                string newfileName=txt_prefix.Text + i;
 
-            CultureInfo StrokCi = new CultureInfo(133124);
-            System.Threading.Thread.CurrentThread.CurrentCulture = StrokCi;
+                fileNameWithDesciption.Add(newfileName, fis[i].Name);
+
+                RenameFile(fis[i], newfileName);
+                
+            }
+            AddFileToDB(fileNameWithDesciption);
+
+
+            dataGridView1.DataSource = ReadData();
+
+            //CultureInfo StrokCi = new CultureInfo(133124);
+            //System.Threading.Thread.CurrentThread.CurrentCulture = StrokCi;
             
-            nameList.Sort(new AlphanumComparatorFast());
+            //nameList.Sort(new AlphanumComparatorFast());
 
 
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < nameList.Count; i++)
-            {
-                sb.Append(nameList[i] + "\r\n");
-                //sb.Append(string.Format("insert into PictureSet(PictureName,pictureDescription) values('{0}','{1}')", "gallary" + i, nameList[i]));
+            //StringBuilder sb = new StringBuilder();
+            //for (int i = 0; i < nameList.Count; i++)
+            //{
+            //    sb.Append(nameList[i] + "\r\n");
+            //    //sb.Append(string.Format("insert into PictureSet(PictureName,pictureDescription) values('{0}','{1}')", "gallary" + i, nameList[i]));
 
-                //sb.Append("\r\n");
+            //    //sb.Append("\r\n");
 
 
 
-            }
+            //}
 
-            textBox2.Text = sb.ToString();
+            //textBox2.Text = sb.ToString();
 
 
         }
+
+        private void RenameFile(FileInfo fileInfo,string newName)
+        {
+            string path = fileInfo.DirectoryName;
+            string ext = fileInfo.Extension;
+
+            fileInfo.CopyTo(Path.Combine(path,newName+ext));
+
+        }
+
+        private DataTable ReadData()
+        {
+            SQLiteDataAdapter ad;
+            SQLiteConnection sqlite = new SQLiteConnection("Data Source=ROC.db3");
+            DataTable dt = new DataTable();
+
+            try
+            {
+                SQLiteCommand cmd;
+                sqlite.Open();  //Initiate connection to the db
+                cmd = sqlite.CreateCommand();
+                cmd.CommandText = "select * from NameDescription";  //set the passed query
+                ad = new SQLiteDataAdapter(cmd);
+                ad.Fill(dt); //fill the datasource
+            }
+            catch (SQLiteException ex)
+            {
+                //Add your exception code here.
+            }
+            sqlite.Close();
+            return dt;
+        }
+
+
+        public static void AddFileToDB(Dictionary<string, string> contenet)
+        {
+            
+            DbProviderFactory factory = SQLiteFactory.Instance;
+            using (DbConnection conn = factory.CreateConnection())
+            {
+                // 连接数据库
+                conn.ConnectionString = "Data Source=ROC.db3";
+                conn.Open();
+
+                // 创建数据表
+               
+                DbCommand cmd = conn.CreateCommand();
+                
+                // 添加参数
+                //cmd.Parameters.Add(cmd.CreateParameter());
+
+
+
+                DbTransaction trans = conn.BeginTransaction(); // <-------------------
+                try
+                {
+                    // 连续插入1000条记录
+                    foreach (var item in contenet)
+	                {
+                        cmd.CommandText = string.Format("insert into NameDescription(FileName,Description) values ('{0}','{1}')",item.Key,item.Value);
+                       
+
+                        cmd.ExecuteNonQuery();
+	                }
+
+                    trans.Commit(); // <-------------------
+                }
+                catch
+                {
+                    trans.Rollback(); // <-------------------
+                    throw; // <-------------------
+                }
+
+            }
+       
+        }
+
     }
 }
